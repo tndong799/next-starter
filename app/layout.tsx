4ee37ed89a6e9f0d9e4890b569b7417be6fb405d
dongtn1 <dongtn1@fireapps.vn>
2024-11-15 18:50:49 +0700

    update-gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/app/ui/global.css';
 import { inter } from './ui/fonts';
+import { Analytics } from './lib/analytics';
 
 export default function RootLayout({
     children,
@@ -8,7 +9,9 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={`${inter.className} antialiased`}>{children}</body>
+            <body className={`${inter.className} antialiased`}>
+                <Analytics>{children}</Analytics>
+            </body>
         </html>
     );
 }
