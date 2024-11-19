5d86227059de8466cd55895a71639d733b910cc2
dongtn1 <dongtn1@fireapps.vn>
2024-11-19 09:05:18 +0700

    update

--- app/components/main-layout.tsx
+++ app/components/main-layout.tsx
@@ -1,12 +1,15 @@
 'use client';
 import { useState } from 'react';
+import useDeviceResponsive from '../hooks/useDevice';
 
 export const MainLayout = ({ children }: { children: React.ReactNode }) => {
     const [count, setCount] = useState(0);
+    const { isMobile } = useDeviceResponsive();
     return (
         <div>
             <button onClick={() => setCount(count + 1)}>Click </button>
             <div>{count}</div>
+            <div>{isMobile ? 'Mobile' : 'Desktop'}</div>
             {children}
         </div>
     );
---
+++ app/hooks/useDevice.ts
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+const useDeviceResponsive = () => {
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 1024);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 1024);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+    return { isMobile };
+};
+
+export default useDeviceResponsive;
